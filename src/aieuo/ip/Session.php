9d5663ac4716b2a50e405c2b63926549baa5988f
aieuo <35859665+aieuo@users.noreply.github.com>
2019-05-03 19:58:28 +0900

    コード整理

--- src/aieuo/ip/Session.php
+++ src/aieuo/ip/Session.php
@@ -5,62 +5,49 @@ namespace aieuo\ip;
 use pocketmine\Player;
 
 class Session {
-
-	const BLOCK = 0;
-	const COMMAND = 1;
-	const EVENT = 2;
-	const CHAIN = 3;
-
     private static $sessions = [];
 
     /**
      * @param  Player $player
-     * @return Session
+     * @return Session | null
      */
-    public static function get(Player $player) : Session {
-        if(!isset(self::$sessions[$player->getName()])) self::$sessions[$player->getName()] = new Session();
+    public static function get(Player $player): Session {
+        if(!isset(self::$sessions[$player->getName()])) return null;
         return self::$sessions[$player->getName()];
     }
 
+    public static function register(Player $player) {
+        self::$sessions[$player->getName()] = new Session();
+    }
 
-	private $valid = false;
-	private $if_type = self::BLOCK;
-	private $datas = [];
+////////////////////////////////////////////////////////////////////////
 
-	public function isValid() {
-		return $this->valid;
-	}
+    /** @var bool */
+    private $valid = false;
+    /** @var array */
+    private $datas = [];
 
-	public function setValid($valid = true, $del = true) : self {
-		$this->valid = $valid;
-		if(!$valid and $del) $this->removeAllData();
-		return $this;
-	}
+    public function setValid($valid = true, $deleteDatas = true): self {
+        $this->valid = $valid;
+        if(!$valid and $deleteDatas) $this->removeAllData();
+        return $this;
+    }
 
-	public function getIfType() : int {
-		return $this->if_type;
-	}
+    public function getData($key, $default = null) {
+        if(!isset($this->datas[$key])) return $default;
+        return $this->datas[$key];
+    }
 
-	public function setIfType($type) : self {
-		$this->if_type = $type;
-		return $this;
-	}
+    public function setData($key, $data): self {
+        $this->datas[$key] = $data;
+        return $this;
+    }
 
-	public function getData($id, $default = null) {
-		if(!isset($this->datas[$id])) return $default;
-		return $this->datas[$id];
-	}
+    public function removeData($key) {
+        unset($this->datas[$key]);
+    }
 
-	public function setData($id, $data) : self {
-		$this->datas[$id] = $data;
-		return $this;
-	}
-
-	public function removeData($id) {
-		unset($this->datas[$id]);
-	}
-
-	public function removeAllData() {
-		$this->datas = [];
-	}
+    public function removeAllData() {
+        $this->datas = [];
+    }
 }
